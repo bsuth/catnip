b21bd28da079cfdc383674e2667b0835588ffa0c
bsuth <bsuth701@gmail.com>
2024-08-18 14:09:55 +0900

    add builtin keybindings for reload / quit

--- src/desktop/key_event.c
+++ src/desktop/key_event.c
@@ -1,6 +1,57 @@
 #include "key_event.h"
 #include "compositor/backend.h"
+#include "compositor/display.h"
+#include "config.h"
+#include <wlr/types/wlr_keyboard.h>
 #include <xkbcommon/xkbcommon-keysyms.h>
+
+static bool
+check_reload_keybinding(struct catnip_key_event* event)
+{
+  if (event->xkb_keysym != XKB_KEY_r) {
+    return false;
+  }
+
+  if (!(event->modifiers & WLR_MODIFIER_CTRL)) {
+    return false;
+  }
+
+  if (!(event->modifiers & WLR_MODIFIER_ALT)) {
+    return false;
+  }
+
+  if (!(event->modifiers & WLR_MODIFIER_LOGO)) {
+    return false;
+  }
+
+  catnip_config_request_reload = true;
+
+  return true;
+}
+
+static bool
+check_quit_keybinding(struct catnip_key_event* event)
+{
+  if (event->xkb_keysym != XKB_KEY_q) {
+    return false;
+  }
+
+  if (!(event->modifiers & WLR_MODIFIER_CTRL)) {
+    return false;
+  }
+
+  if (!(event->modifiers & WLR_MODIFIER_ALT)) {
+    return false;
+  }
+
+  if (!(event->modifiers & WLR_MODIFIER_LOGO)) {
+    return false;
+  }
+
+  catnip_display_run = false;
+
+  return true;
+}
 
 static bool
 check_vt_keybindings(struct catnip_key_event* event)
@@ -30,7 +81,11 @@ catnip_key_event_check_keybindings(struct catnip_key_event* event)
     return false;
   }
 
-  if (check_vt_keybindings(event)) {
+  if (check_reload_keybinding(event)) {
+    return true;
+  } else if (check_quit_keybinding(event)) {
+    return true;
+  } else if (check_vt_keybindings(event)) {
     return true;
   }
 
