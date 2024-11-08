10effe0cf151e9bd408d406dcb186a439edfb465
bsuth <bsuth701@gmail.com>
2024-11-08 17:17:17 +0900

    add output wallpaper support

--- src/compositor/scene.c
+++ src/compositor/scene.c
@@ -1,9 +1,11 @@
 #include "scene.h"
 
 struct wlr_scene* catnip_scene = NULL;
+struct wlr_scene_tree* catnip_scene_wallpaper_layer = NULL;
 
 void
 catnip_scene_init()
 {
   catnip_scene = wlr_scene_create();
+  catnip_scene_wallpaper_layer = wlr_scene_tree_create(&catnip_scene->tree);
 }
