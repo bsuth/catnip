5a0a0ed2cc7eb68169d881d7a672bb80d1923637
bsuth <bsuth701@gmail.com>
2024-11-08 17:21:17 +0900

    add scene desktop layer (windows + widgets)

--- src/compositor/scene.c
+++ src/compositor/scene.c
@@ -2,10 +2,12 @@
 
 struct wlr_scene* catnip_scene = NULL;
 struct wlr_scene_tree* catnip_scene_wallpaper_layer = NULL;
+struct wlr_scene_tree* catnip_scene_desktop_layer = NULL;
 
 void
 catnip_scene_init()
 {
   catnip_scene = wlr_scene_create();
   catnip_scene_wallpaper_layer = wlr_scene_tree_create(&catnip_scene->tree);
+  catnip_scene_desktop_layer = wlr_scene_tree_create(&catnip_scene->tree);
 }
